206156f587edf2bf0cf6982d4811ad8cc944667b
aristotelesalves <arystotelys@gmail.com>
2023-10-15 16:22:25 -0300

    página dispesas

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.css'
 import { Home } from './pages/Home.tsx'
+import { Dispesas } from './pages/Dispesas.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home/>
+    <Dispesas/>
   </React.StrictMode>,
 )
